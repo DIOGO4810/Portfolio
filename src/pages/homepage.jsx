142b05af7788470ac29461b6eede3a4cfaeda429
DIOGO4810 <ribeiroeribeiro2005@gmail.com>
2025-02-20 16:06:53 +0000

    Implementaçao de algumas animaçoes

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,17 +1,22 @@
+import { useState } from 'react';
 import '../CSS/pages.css';
 
 function Homepage() {
+
+
+
+
     return (
         <div>
             <h1 className="title">Home</h1>
             <div className="grid-parent">
-                <div className="container">
+                <div className="container size-1">
                     <p className="text">Hello i am Diogo a Student in Universidade do Minho neste momento no segundo ano com uma imensa curiosidade a aprender ainda mais sobre programas</p>
                 </div>
-                <div className="container">
+                <div className="container size-2">
                     <p className="text">Hello i am Diogo a Student in Universidade do Minho neste momento no segundo ano com uma imensa curiosidade a aprender ainda mais sobre programas</p>
                 </div>
-                <div className="container">
+                <div className="container size-3">
                     <p className="text">Hello i am Diogo a Student in Universidade do Minho neste momento no segundo ano com uma imensa curiosidade a aprender ainda mais sobre programas</p>
                 </div>
                 <div className="container">
