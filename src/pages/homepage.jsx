1ed5a67baaf70e7998ac0f636269e5b13b63ecec
DIOGO4810 <ribeiroeribeiro2005@gmail.com>
2025-02-23 18:58:44 +0000

    Finalização da pagina de competencias e projetos

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "../CSS/pages.css";
+import { Icon } from "@iconify/react";
 
 function Homepage() {
   const [clicked, setClicked] = useState(false);
@@ -14,13 +15,28 @@ function Homepage() {
       <div className="grid-parent">
         <div className="containerAbout size-1">
           <h1 className="title titleAbout">About me</h1>
-          <p className="text ">Olá, chamo-me Diogo  👋<br /> <br />
+          <p className="text ">
+            Olá, chamo-me Diogo 👋
+            <br /> <br />
             Sou estudante de Engenharia Informática na Universidade do Minho e
             desde o 9º ano que a programação desperta a minha curiosidade. Vivo
             em Braga, na freguesia de Palmeira, e gosto de dividir o meu tempo
             entre os estudos e outras atividades que me motivam. Para além da
             programação, correr é uma das minhas grandes paixões, ajudando-me a
             manter o equilíbrio entre o foco e o bem-estar.
+            <br />
+            
+            <a href="https://github.com/DIOGO4810">              
+              <Icon icon = "devicon:github" width="25" height="25"/>
+            </a>
+                      
+            <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=GTvVlcRwRrkDpKgLLTzWVvcqGNDRxBTlTdTXnLWJTKkThVPzLKlNGBDHPzsXjmqcVNFQgSSFrxcJC">
+              <Icon icon = "logos:google-gmail" width="25" height="25"/>
+            </a>
+            
+            <a href="https://www.linkedin.com/in/diogo-ribeiro-71742420b">
+              <Icon icon = "logos:linkedin-icon" width="25" height="25"/>
+            </a>
           </p>
           <img src="Diogo.jpg" className="imagem" alt="Diogo" />
         </div>
--- src/pages/projetos.jsx
+++ src/pages/projetos.jsx
@@ -1,27 +1,105 @@
-import { useState } from 'react';
-import '../CSS/pages.css';
-
+import { useState } from "react";
+import "../CSS/pages.css";
+import { Icon } from "@iconify/react";
 function Projetos() {
+  const [clicked, setClicked] = useState(false);
 
-    const [clicked,setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked((prev) => !prev);
+  };
 
-    const handleClick = () => {
-        setClicked(prev => !prev);
-    }
-    
-    return (
-        <div className='body' onClick={handleClick}>
-            <h1 className="title">Home</h1>
-            <div className="grid-parent" >
-                <div className={`container size-1 ${clicked ? "containerCenter" : ""}`} >
-                    <p className="text">projeots</p>
-                </div>
+  return (
+    <div className="body">
+      <h1 className="title">Projetos</h1>
+      <div className="grid-parent">
+        <div className="row">
+          <div className="container size-2">
+            <h2 className="title">Donkey Kong em haskell</h2>
+            <p className="text">
+              Este projeto recriou o jogo Donkey Kong usando Haskell e a
+              biblioteca Gloss para gráficos. O objetivo foi aprender
+              programação funcional, aplicando conceitos como funções puras e
+              gestão de estados imutáveis. Utilizou-se Gloss para desenhar
+              gráficos 2D e animar o jogo, e o projeto permitiu explorar os
+              desafios de implementar jogos sem usar loops imperativos, típicos
+              de linguagens imperativas.br
+            </p>
+            <br />
+            <a href="https://github.com/DIOGO4810/LI1">
+              <Icon
+                icon="devicon:github"
+                width="25"
+                height="25"
+                className="centraIcons"
+              />
+            </a>
+          </div>
 
-                
+          <div className="container size-2">
+            <h2 className="title">Backend do spotify</h2>
+            <p className="text">
+              Projeto em C que envolveu ler arquivos CSV e armazenar informações
+              em estruturas de dados eficientes. O foco foi criar queries que
+              permitissem acessar dados de maneira rápida e eficiente, como
+              buscar músicas, artistas ou informações de usuários. O projeto
+              proporcionou um grande aprendizado sobre eficiência de algoritmos
+              e o uso de estruturas de dados para manipular grandes volumes de
+              dados.
+            </p>
+            <a href="https://github.com/DIOGO4810/LI3">
+              <Icon
+                icon="devicon:github"
+                width="25"
+                height="25"
+                className="centraIcons"
+              />
+            </a>
+          </div>
 
-            </div>
+          <div className="container size-2">
+            <h2 className="title">Gerenciador de Recursos</h2>
+            <p className="text">
+              Projeto em C que centraliza informações do PC em uma única
+              ferramenta. Em vez de precisar abrir várias aplicações para
+              verificar dados como consumo de bateria, uso de RAM, memória
+              disponível, utilização do processador e tempo ligado, o projeto
+              fornece acesso a todos esses recursos em uma interface simples. A
+              ferramenta visa facilitar o monitoramento do desempenho e consumo
+              do sistema, sem a necessidade de múltiplas aplicações.
+            </p>
+            <a href="https://github.com/DIOGO4810/Gerenciador-de-Recursos">
+              <Icon
+                icon="devicon:github"
+                width="25"
+                height="25"
+                className="centraIcons"
+              />
+            </a>
+          </div>
+
+          <div className="container size-2">
+            <h2 className="title">Portfólio</h2>
+            <p className="text">
+              Site criado com React, JavaScript, HTML e CSS, com a intenção de
+              ser uma extensão do meu CV. O objetivo foi oferecer uma visão mais
+              completa sobre quem sou, destacando minhas habilidades e projetos
+              de forma interativa. Além disso, o projeto me permitiu aprender e
+              aplicar o processo de deploy de uma aplicação web, garantindo que
+              o site estivesse acessível online para qualquer visitante.
+            </p>
+            <a href="https://github.com/DIOGO4810/Portfolio">
+              <Icon
+                icon="devicon:github"
+                width="25"
+                height="25"
+                className="centraIcons"
+              />
+            </a>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Projetos;
